d400f51f0db79741350efa163fdd96c76ea25d12
Aung Thiha <mr.aungthiha@gmail.com>
2024-05-13 21:41:29 +0700

    feat: use uuid for userid instead of simple integer

--- config/auth.config.js
+++ config/auth.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  // jwtExpiration: 86400,           // 24 hours
-  // jwtRefreshExpiration: 2592000,   // 30 days
-  jwtExpiration: 300,           // 1 minute
-  jwtRefreshExpiration: 86400,   // 30 days
+  jwtExpiration: 180,           // 3 minutes
+  jwtRefreshExpiration: 2592000,   // 30 days
 };
