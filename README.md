c50f623acca2cf7a82cfce5832dd9b99d1cc9107
Aung Thiha <mr.aungthiha@gmail.com>
2024-11-18 16:06:01 +0700

    docs: update readme

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # PhotoAlbumServer
 Express.js Backend Application for a Photo Album App
-
-##  To make this development ready
-- remove `db.sync` from `app.js`. There's a comment why it needs to be removed
--- config/auth.config.js
+++ config/auth.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  jwtExpiration: 180,           // 3 minutes
-  jwtRefreshExpiration: 2592000,   // 30 days
+  jwtExpiration: 30,           // 3 minutes
+  jwtRefreshExpiration: 60,   // 30 days
 };
