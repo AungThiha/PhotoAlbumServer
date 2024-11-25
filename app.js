0834f604cec0816bf76c0c8519b9df45c636f81a
Aung Thiha <mr.aungthiha@gmail.com>
2024-11-25 18:58:21 +0700

    feat: remove resyncing db

--- app.js
+++ app.js
@@ -15,12 +15,6 @@ app.use(
 )
 app.use('/photos', express.static('photos'))
 
-// sync shouldn't be used in production
-// see https://sequelize.org/docs/v7/models/migrations/ for db migration in production
-db.sequelize.sync({force: true}).then(() => {
-  console.log('Drop and Resync Db');
-});
-
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
 })
